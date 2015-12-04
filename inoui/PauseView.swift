c5c9cb55b8068eb2c3d7e5d18c4f6eaf1900cd2e
Chloé Henaut <c3henaut@gmail.com>
2015-12-04 14:50:46 +0100

    Pause state

---
+++ inoui/PauseView.swift
@@ -0,0 +1,21 @@
+//
+//  PauseView.swift
+//  inoui
+//
+//  Created by Chloé Henaut on 04/12/2015.
+//  Copyright © 2015 Inoui. All rights reserved.
+//
+
+import UIKit
+
+class PauseView: UIView {
+    
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+    // Drawing code
+    }
+    */
+    
+}
